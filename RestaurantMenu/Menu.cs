66fae35ae0ff2f664fe0ade6de6f14703b28cb89
Janell Renee Johnson <docj4jesus@gmail.com>
2020-10-06 20:45:08 -0500

    Initial commit

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        //have a menu items list set to public...iterate through in program later
+
+        public DateTime LastUpdated { get; }
+        public List<MenuItem> Items { get; set; }
+
+        public Menu(DateTime dateUpdated, List<MenuItem> item)
+        {
+            LastUpdated = dateUpdated;
+            Items = item;
+        }
+
+
+    }
+}
